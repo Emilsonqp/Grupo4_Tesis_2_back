dc073e85e6ee7f84ca88a6c602dffcc35e720f0a
William Ravelo M <drummerwilliam@gmail.com>
2023-03-12 10:42:57 -0500

    fix

--- src/routes/consult_routes.py
+++ src/routes/consult_routes.py
@@ -62,7 +62,7 @@ def update(id):
     consult = UpdateConsultStatus(id, request.get_json()).execute()
     return jsonify(consult)
 
-@consult_routes.route('/consults_update/<id>', methods=['PUT'])
+@consult_routes.route('/consults_update/<remote_id>', methods=['PUT'])
 @jwt_required()
 def update_consult(remote_id):
     current_email = get_jwt_identity()
